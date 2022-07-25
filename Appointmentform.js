f20430cc45847507666ce398929b06f558f0d15e
Shivamkumar098 <shivam152000kmr@gmail.com>
2022-07-25 23:32:50 +0530

    Appointment-Form

--- Appointmentform.js
+++ Appointmentform.js
@@ -7,6 +7,9 @@ Appointmentform.addEventListener("submit", function(e){
      var First3=Appointmentform.elements[`First3`].value;
      var First4=Appointmentform.elements[`First4`].value;
      var First5=Appointmentform.elements[`First5`].value;
+     var question1 = Appointmentform.elements[`Eligible`].value;
+     var question2 = Appointmentform.elements[`Eligible1`].value;
+     var question3 = Appointmentform.elements[`Eligible2`].value;
 
      
     // console.log(loginform.elements[`username`].value)
@@ -28,7 +31,8 @@ Appointmentform.addEventListener("submit", function(e){
         First2,
         First3,
         First4,
-        First5
+        First5,
+        question1,question2,question3
     }
 
     console.log(JSON.stringify(AppointmentData));
